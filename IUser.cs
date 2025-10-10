578b912119b5ff810318355ba6e9c64125cc2a4f
M-Renberg <mikael.k.renberg@hotmail.com>
2025-10-10 09:44:58 +0200

    Added IUser and namespace
    Created the basic IUser and set a namespace

---
+++ IUser.cs
@@ -0,0 +1,17 @@
+namespace HospitalApp;
+
+    interface IUser
+    {
+        public bool IsRole(Role role);
+
+        public bool TryLogin(string username, string password);
+    }
+
+    enum Role
+    {
+        Patient,
+        Admin,
+        Personnel,
+
+
+    }
