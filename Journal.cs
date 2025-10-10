b9f0f39a8491bd1e6e6171a0654e878a380bf07b
M-Renberg <mikael.k.renberg@hotmail.com>
2025-10-10 11:22:11 +0200

    Read permissions
    started working on Read Permissions

--- Journal.cs
+++ Journal.cs
@@ -1,15 +1,28 @@
 namespace HospitalApp;
 
-class Journal
+class JournalEntry
 {
+    //id på journalen?
     public string PatientID;
-    public string AutherID;
+    public string DoctorID;
     public string Text;
+    //tid när den skrevs?
+    public ReadPermisson readPermisson; //read permissions?
 
-    public Journal(string patientid, string autherid, string text)
+    public JournalEntry(string patientid, string doctorid, string text)
     {
         PatientID = patientid;
-        AutherID = autherid;
+        DoctorID = doctorid;
         Text = text;
+
+        readPermisson = ReadPermisson.None; //börja som None. set när man lägger in en entry
+    }
+
+    public enum ReadPermisson
+    {
+        None,
+        PatientRead, //public? alla kan läsa?
+        StaffRead, // dom som jobbar på sjukhuset?
+        DoctorRead, // Bara läkare kan läsa
     }
 }
