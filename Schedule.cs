1c5b8cc7e80a0b5fc43f4f03311051c96837793d
M-Renberg <mikael.k.renberg@hotmail.com>
2025-10-14 10:37:25 +0200

    schedule will now be filled
    will be fillend when it's lunch break and no appointments
    
    print funtion added

--- Schedule.cs
+++ Schedule.cs
@@ -8,19 +8,40 @@ class Schedule
 
     public Schedule()
     {
+        FilledNullOnSchedule();
+    }
 
+    public void FilledNullOnSchedule()
+    {
+        string Lunch = "Lunch";
+        WeekSchedule[0,5] = Lunch;
+        WeekSchedule[1,5] = Lunch;
+        WeekSchedule[2,5] = Lunch;
+        WeekSchedule[3,5] = Lunch;
+        WeekSchedule[4,5] = Lunch;
+
+        for(int i = 0; i < WeekSchedule.GetLength(0); i++)
+        {
+            for(int j = 0; j < WeekSchedule.GetLength(1); j++)
+            {
+                if(WeekSchedule[i,j] == null)
+                {
+                    WeekSchedule[i,j] = "No Appointment";
+                }
+            }
+        }
     }
     
     public void PrintSchedule()
     {
-        Console.WriteLine($"|-Monday-|-Tuseday-|-Wednesday-|-Thursday-|-Friday-|");
-        Console.WriteLine($"|-{WeekSchedule[0,0]}-|-{WeekSchedule[1,0]}-|-{WeekSchedule[2,0]}-|-{WeekSchedule[3,0]}-|-{WeekSchedule[4,0]}-|");
-        Console.WriteLine($"|-{WeekSchedule[0,1]}-|-{WeekSchedule[1,1]}-|-{WeekSchedule[2,1]}-|-{WeekSchedule[3,1]}-|-{WeekSchedule[4,1]}-|");
-        Console.WriteLine($"|-{WeekSchedule[0,2]}-|-{WeekSchedule[1,2]}-|-{WeekSchedule[2,2]}-|-{WeekSchedule[3,2]}-|-{WeekSchedule[4,2]}-|");
-        Console.WriteLine($"|-{WeekSchedule[0,3]}-|-{WeekSchedule[1,3]}-|-{WeekSchedule[2,3]}-|-{WeekSchedule[3,3]}-|-{WeekSchedule[4,3]}-|");
-        Console.WriteLine($"|-{WeekSchedule[0,4]}-|-{WeekSchedule[1,4]}-|-{WeekSchedule[2,4]}-|-{WeekSchedule[3,4]}-|-{WeekSchedule[4,4]}-|");
-        Console.WriteLine($"|-{WeekSchedule[0,5]}-|-{WeekSchedule[1,5]}-|-{WeekSchedule[2,5]}-|-{WeekSchedule[3,5]}-|-{WeekSchedule[4,5]}-|");
-        Console.WriteLine($"|-{WeekSchedule[0,6]}-|-{WeekSchedule[1,6]}-|-{WeekSchedule[2,6]}-|-{WeekSchedule[3,6]}-|-{WeekSchedule[4,6]}-|");
+        Console.WriteLine($"Day:  |-Monday-|-Tuseday-|-Wednesday-|-Thursday-|-Friday-|");
+        Console.WriteLine($"8.00: |-{WeekSchedule[0,0]}-|-{WeekSchedule[1,0]}-|-{WeekSchedule[2,0]}-|-{WeekSchedule[3,0]}-|-{WeekSchedule[4,0]}-|");
+        Console.WriteLine($"9.00: |-{WeekSchedule[0,1]}-|-{WeekSchedule[1,1]}-|-{WeekSchedule[2,1]}-|-{WeekSchedule[3,1]}-|-{WeekSchedule[4,1]}-|");
+        Console.WriteLine($"10.00:|-{WeekSchedule[0,2]}-|-{WeekSchedule[1,2]}-|-{WeekSchedule[2,2]}-|-{WeekSchedule[3,2]}-|-{WeekSchedule[4,2]}-|");
+        Console.WriteLine($"11.00:|-{WeekSchedule[0,3]}-|-{WeekSchedule[1,3]}-|-{WeekSchedule[2,3]}-|-{WeekSchedule[3,3]}-|-{WeekSchedule[4,3]}-|");
+        Console.WriteLine($"12.00:|-{WeekSchedule[0,4]}-|-{WeekSchedule[1,4]}-|-{WeekSchedule[2,4]}-|-{WeekSchedule[3,4]}-|-{WeekSchedule[4,4]}-|");
+        Console.WriteLine($"13.00:|-{WeekSchedule[0,5]}-|-{WeekSchedule[1,5]}-|-{WeekSchedule[2,5]}-|-{WeekSchedule[3,5]}-|-{WeekSchedule[4,5]}-|");
+        Console.WriteLine($"14.00:|-{WeekSchedule[0,6]}-|-{WeekSchedule[1,6]}-|-{WeekSchedule[2,6]}-|-{WeekSchedule[3,6]}-|-{WeekSchedule[4,6]}-|");
 
     }
 
