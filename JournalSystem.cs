9be46c9454ac594343991528d2c45ff119784b49
M-Renberg <mikael.k.renberg@hotmail.com>
2025-10-21 12:45:36 +0200

    Journalsystem add function
    relezed alot was missing that I was meant to code.
    fixed it and will try it now

--- JournalSystem.cs
+++ JournalSystem.cs
@@ -3,7 +3,7 @@ namespace HospitalApp;
 class JournalSystem //logic for handeling the journal and journal entries
 {
 
-    public void AddJournalEntry(List<JournalEntry> journals, List<IUser> users, IUser? activeUser) //add journal entry method. using the journals list, users list, and who is active user
+    public void AddJournalEntry(List<JournalEntry> journals, List<IUser> users, IUser? activeUser, (string patientslot, string doctorslot)[,] WeekSchedule) //add journal entry method. using the journals list, users list, and who is active user
     {
         Console.Clear(); //clear the screen
         Console.WriteLine("Adding new journal entry:"); //text
@@ -13,67 +13,145 @@ class JournalSystem //logic for handeling the journal and journal entries
         System.Console.WriteLine("[2] Browers patients"); //giving the two options
         string? AJEInput = Console.ReadLine(); //getting a string to work with
 
-        if(AJEInput == "1") //option 1
+        if (AJEInput == "1") //option 1
         {
-            foreach(IUser user in users)
+            foreach (JournalEntry je in journals)
+            {
+                if (je.Personnel.ToString() == activeUser!.ToString())
+                {
+                    System.Console.WriteLine($"Patient: {je.Patient}");
+                }
+
+                for (int i = 0; i < WeekSchedule.GetLength(0); i++)
+                {
+                    for (int j = 0; j < WeekSchedule.GetLength(1); j++)
+                    {
+                        if (WeekSchedule[i, j].doctorslot == activeUser!.ToString())
+                        {
+                            System.Console.WriteLine($"{WeekSchedule[i, j].patientslot}");
+                        }
+                    }
+                }
+                
+            }
+            Console.WriteLine("Write the name of the patient you wish to make an entry for");
+
+            string? PatientInput = Console.ReadLine(); //string to use to get the right patient.
+            if (PatientInput == null) //it it's null
+            {
+                System.Console.WriteLine("Wrong input");
+                Console.ReadLine();
+                return;
+            }
+
+            Patient? patientuser = users.OfType<Patient>().FirstOrDefault(p => p.Email == PatientInput); // creating a variable to use as the patient by checking the users list. first the linq sort the list by user type (patient), the it looks up the first patient (email) that matches the string input.
+
+            if (patientuser == null) //if it's null
+            {
+                System.Console.WriteLine("Patient do not exist");
+                Console.ReadLine();
+                return;
+            }
+
+            System.Console.WriteLine($"You have selected {patientuser!.Email}"); //sanity check to see if it's the right patient
+            System.Console.WriteLine("Write documentaion:");
+            string? TextInput = Console.ReadLine(); //input for the text documentaion
+            if (TextInput == null) //if it's null
+            {
+                System.Console.WriteLine("Something went wrong");
+                Console.ReadLine();
+                return;
+            }
+            System.Console.WriteLine("You have succesfully entered the documentaion"); //sanity check 
+
+            System.Console.WriteLine("Select read permission:");
+            System.Console.WriteLine("[1] Patient lvl permission"); //sets the read permission to patient och personnel
+            System.Console.WriteLine("[2] Personnel lvl permission"); //sets the read permission to only personnel
+
+            switch (Console.ReadLine())
+            {
+                case "1": //sets the read permission to patient och personnel.... by using the patient variable from earlier, who the active user is, text documentaion string and enum read permission.
+                    journals.Add(new JournalEntry(patientuser, activeUser!, TextInput!, JournalEntry.ReadPermisson.PatientRead | JournalEntry.ReadPermisson.PersonnelRead));
+                    break;
+                case "2":
+                    journals.Add(new JournalEntry(patientuser, activeUser!, TextInput!, JournalEntry.ReadPermisson.PersonnelRead));
+                    break;
+                default:
+                    System.Console.WriteLine("Something went wrong"); //if you do an incorrent choice
+                    Console.ReadLine();
+                    break;
+            }
+
+            System.Console.WriteLine("text that it is finished"); //sanity check to see that it worked.
+            Console.ReadLine();
+
+        }
+        if (AJEInput == "2") //option 2
+        {
+            foreach (IUser user in users)
             {
                 if (user.IsRole(Role.Patient))
                 {
                     System.Console.WriteLine($"Patient: {user.ToString()}");
                 }
             }
-        Console.WriteLine("Write the name of the patient you wish to make an entry for");
+            Console.WriteLine("Write the name of the patient you wish to make an entry for");
 
-        string? PatientInput = Console.ReadLine(); //string to use to get the right patient.
-        if(PatientInput == null) //it it's null
-        {
-            System.Console.WriteLine("Wrong input");
+            string? PatientInput = Console.ReadLine(); //string to use to get the right patient.
+            if (PatientInput == null) //it it's null
+            {
+                System.Console.WriteLine("Wrong input");
+                Console.ReadLine();
+                return;
+            }
+
+            Patient? patientuser = users.OfType<Patient>().FirstOrDefault(p => p.Email == PatientInput); // creating a variable to use as the patient by checking the users list. first the linq sort the list by user type (patient), the it looks up the first patient (email) that matches the string input.
+
+            if (patientuser == null) //if it's null
+            {
+                System.Console.WriteLine("Patient do not exist");
+                Console.ReadLine();
+                return;
+            }
+
+            System.Console.WriteLine($"You have selected {patientuser!.Email}"); //sanity check to see if it's the right patient
+            System.Console.WriteLine("Write documentaion:");
+            string? TextInput = Console.ReadLine(); //input for the text documentaion
+            if (TextInput == null) //if it's null
+            {
+                System.Console.WriteLine("Something went wrong");
+                Console.ReadLine();
+                return;
+            }
+            System.Console.WriteLine("You have succesfully entered the documentaion"); //sanity check 
+
+            System.Console.WriteLine("Select read permission:");
+            System.Console.WriteLine("[1] Patient lvl permission"); //sets the read permission to patient och personnel
+            System.Console.WriteLine("[2] Personnel lvl permission"); //sets the read permission to only personnel
+
+            switch (Console.ReadLine())
+            {
+                case "1": //sets the read permission to patient och personnel.... by using the patient variable from earlier, who the active user is, text documentaion string and enum read permission.
+                    journals.Add(new JournalEntry(patientuser, activeUser!, TextInput!, JournalEntry.ReadPermisson.PatientRead | JournalEntry.ReadPermisson.PersonnelRead));
+                    break;
+                case "2":
+                    journals.Add(new JournalEntry(patientuser, activeUser!, TextInput!, JournalEntry.ReadPermisson.PersonnelRead));
+                    break;
+                default:
+                    System.Console.WriteLine("Something went wrong"); //if you do an incorrent choice
+                    Console.ReadLine();
+                    break;
+            }
+
+            System.Console.WriteLine("text that it is finished"); //sanity check to see that it worked.
             Console.ReadLine();
-            return;
+
         }
-
-        Patient? patientuser = users.OfType<Patient>().FirstOrDefault(p => p.Email == PatientInput); // creating a variable to use as the patient by checking the users list. first the linq sort the list by user type (patient), the it looks up the first patient (email) that matches the string input.
-
-        if(patientuser == null) //if it's null
-        {
-            System.Console.WriteLine("Patient do not exist");
-            Console.ReadLine();
-            return;
-        }
-
-        System.Console.WriteLine($"You have selected {patientuser!.Email}"); //sanity check to see if it's the right patient
-        System.Console.WriteLine("Write documentaion:");
-        string? TextInput = Console.ReadLine(); //input for the text documentaion
-        if(TextInput == null) //if it's null
+        else
         {
             System.Console.WriteLine("Something went wrong");
             Console.ReadLine();
-            return;
-        }
-        System.Console.WriteLine("You have succesfully entered the documentaion"); //sanity check 
-
-        System.Console.WriteLine("Select read permission:");
-        System.Console.WriteLine("[1] Patient lvl permission"); //sets the read permission to patient och personnel
-        System.Console.WriteLine("[2] Personnel lvl permission"); //sets the read permission to only personnel
-
-        switch (Console.ReadLine())
-        {
-            case "1": //sets the read permission to patient och personnel.... by using the patient variable from earlier, who the active user is, text documentaion string and enum read permission.
-                journals.Add(new JournalEntry(patientuser, activeUser!, TextInput!, JournalEntry.ReadPermisson.PatientRead | JournalEntry.ReadPermisson.PersonnelRead));
-                break;
-            case "2":
-                journals.Add(new JournalEntry(patientuser, activeUser!, TextInput!, JournalEntry.ReadPermisson.PersonnelRead));
-                break;
-            default:
-                System.Console.WriteLine("Something went wrong"); //if you do an incorrent choice
-                Console.ReadLine();
-                break;
-        }
-
-        System.Console.WriteLine("text that it is finished"); //sanity check to see that it worked.
-        Console.ReadLine();
-        
-    }    
+        }  
 }
         
         
