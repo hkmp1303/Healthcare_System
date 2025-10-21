6d3eea50a8e80cae2523fe704107848d8556a6e0
M-Renberg <mikael.k.renberg@hotmail.com>
2025-10-21 13:34:45 +0200

    Journalsystem
    fixed it and commented code

--- JournalSystem.cs
+++ JournalSystem.cs
@@ -1,9 +1,11 @@
+using System.Net.Quic;
+
 namespace HospitalApp;
 
 class JournalSystem //logic for handeling the journal and journal entries
 {
 
-    public void AddJournalEntry(List<JournalEntry> journals, List<IUser> users, IUser? activeUser, (string patientslot, string doctorslot)[,] WeekSchedule) //add journal entry method. using the journals list, users list, and who is active user
+    public void AddJournalEntry(List<JournalEntry> journals, List<IUser> users, IUser? activeUser, (string patientslot, string doctorslot)[,] WeekSchedule) //add journal entry method. using the journals list, users list, and who is active user, and the WeekSchedule
     {
         Console.Clear(); //clear the screen
         Console.WriteLine("Adding new journal entry:"); //text
@@ -13,27 +15,37 @@ class JournalSystem //logic for handeling the journal and journal entries
         System.Console.WriteLine("[2] Browers patients"); //giving the two options
         string? AJEInput = Console.ReadLine(); //getting a string to work with
 
+        bool PaitentCheck = false; //bool to check if you have any patients
+
         if (AJEInput == "1") //option 1
         {
-            foreach (JournalEntry je in journals)
+            foreach (JournalEntry je in journals) //check journals
             {
-                if (je.Personnel.ToString() == activeUser!.ToString())
+                if (je.Personnel.ToString() == activeUser!.ToString()) //if you have writen about a patient earlier
                 {
-                    System.Console.WriteLine($"Patient: {je.Patient}");
+                    System.Console.WriteLine($"Patient: {je.Patient}"); //print the patient
+                    PaitentCheck = true; //make true if you have writen about a patient ealier
                 }
-
-                for (int i = 0; i < WeekSchedule.GetLength(0); i++)
+                for (int i = 0; i < WeekSchedule.GetLength(0); i++) //check schedule if you ahve patients
                 {
                     for (int j = 0; j < WeekSchedule.GetLength(1); j++)
                     {
-                        if (WeekSchedule[i, j].doctorslot == activeUser!.ToString())
+                        if (WeekSchedule[i, j].doctorslot == activeUser!.ToString()) //if you have a patient this week
                         {
-                            System.Console.WriteLine($"{WeekSchedule[i, j].patientslot}");
+                            System.Console.WriteLine($"{WeekSchedule[i, j].patientslot}"); //print the paient
+                            PaitentCheck = true; //make the bool true
                         }
                     }
                 }
-                
             }
+
+            if (!PaitentCheck) //if the bool never was true (always false)
+            {
+                System.Console.WriteLine("You have no patient to write about");
+                Console.ReadLine();
+                return; //end the method
+            }
+
             Console.WriteLine("Write the name of the patient you wish to make an entry for");
 
             string? PatientInput = Console.ReadLine(); //string to use to get the right patient.
@@ -88,11 +100,11 @@ class JournalSystem //logic for handeling the journal and journal entries
         }
         if (AJEInput == "2") //option 2
         {
-            foreach (IUser user in users)
+            foreach (IUser user in users) //loop out every patient in the system
             {
-                if (user.IsRole(Role.Patient))
+                if (user.IsRole(Role.Patient)) //loop based on role
                 {
-                    System.Console.WriteLine($"Patient: {user.ToString()}");
+                    System.Console.WriteLine($"Patient: {user.ToString()}"); //print
                 }
             }
             Console.WriteLine("Write the name of the patient you wish to make an entry for");
@@ -147,25 +159,25 @@ class JournalSystem //logic for handeling the journal and journal entries
             Console.ReadLine();
 
         }
-        else
+        else //if Something went wrong
         {
             System.Console.WriteLine("Something went wrong");
             Console.ReadLine();
-        }  
-}
+        }
+    }
         
         
         
     
-    public void SeeJournalEntry(List<JournalEntry> journals, List<IUser> users, IUser? activeUser)
+    public void SeeJournalEntry(List<JournalEntry> journals, List<IUser> users, IUser? activeUser) //method to see journal entries. using journals List users List and active user
     {
-        if (activeUser!.IsRole(Role.Patient))
+        if (activeUser!.IsRole(Role.Patient)) //if you're a patient
         {
             Console.Clear();
-            System.Console.WriteLine($"Journal entries for {activeUser.ToString()}");
-            foreach (JournalEntry je in journals)
+            System.Console.WriteLine($"Journal entries for {activeUser.ToString()}"); //just looks nice
+            foreach (JournalEntry je in journals) //loop to get all entries
             {
-                if (activeUser == je.Patient)
+                if (activeUser == je.Patient) //match activeUser with the right entry
                 {
 
                     Console.WriteLine($"Patient {je.Patient}");
@@ -176,30 +188,30 @@ class JournalSystem //logic for handeling the journal and journal entries
             }
             Console.ReadLine();
         }
-        else if (activeUser!.IsRole(Role.Personnel))
+        else if (activeUser!.IsRole(Role.Personnel)) // if you're personnel
         {
             Console.Clear();
             System.Console.WriteLine("Welcome to the Journal System:");
-            System.Console.WriteLine("[1] Search patient journal");
-            System.Console.WriteLine("[2] Browers all entrys you have made");
+            System.Console.WriteLine("[1] Search patient journal"); //look up specifik patient
+            System.Console.WriteLine("[2] Browers all entrys you have made"); //look up everything you have done
             switch (Console.ReadLine())
             {
                 case "1":
 
                     System.Console.WriteLine("Write the Patient you wish to look up journal for");
-                    string? LookUpInput = Console.ReadLine();
+                    string? LookUpInput = Console.ReadLine(); //make an input string
 
-                    var paitenSearch = users.OfType<Patient>().FirstOrDefault(p => p.Email == LookUpInput);
+                    var paitenSearch = users.OfType<Patient>().FirstOrDefault(p => p.Email == LookUpInput); //make a seach variable. sort user list by type and then looks up the first user with a matching user email. 
 
-                    foreach (JournalEntry je in journals)
+                    foreach (JournalEntry je in journals) //loop all journal entries
                     {
-                        if (paitenSearch == je.Patient)
+                        if (paitenSearch == je.Patient) //if it matches the search
                         {
 
                             Console.WriteLine($"Patient {je.Patient}");
                             Console.WriteLine($"Entry: {je.Text}");
                             Console.WriteLine($"Auther: {je.Personnel}");
-                            Console.WriteLine($"Writen: {je.dateTime}");
+                            Console.WriteLine($"Writen: {je.dateTime}"); //print
                         }
                     }
                     Console.ReadLine();
@@ -207,17 +219,17 @@ class JournalSystem //logic for handeling the journal and journal entries
                     break;
                 case "2":
 
-                    if (activeUser!.IsRole(Role.Personnel))
+                    if (activeUser!.IsRole(Role.Personnel)) //if you're personal
                     {
-                        foreach (JournalEntry je in journals)
+                        foreach (JournalEntry je in journals) //loop the list
                         {
-                            if (activeUser == je.Personnel)
+                            if (activeUser == je.Personnel) //if activeUser is the Personnel who wrote the entry
                             {
 
                                 Console.WriteLine($"Patient {je.Patient}");
                                 Console.WriteLine($"Entry: {je.Text}");
                                 Console.WriteLine($"Auther: {je.Personnel}");
-                                Console.WriteLine($"Writen: {je.dateTime}");
+                                Console.WriteLine($"Writen: {je.dateTime}"); //print
                             }
                         }
                     }
@@ -230,7 +242,7 @@ class JournalSystem //logic for handeling the journal and journal entries
         }
         else
         {
-            System.Console.WriteLine("You do not have the right permission to view these files");
+            System.Console.WriteLine("You do not have the right permission to view these files"); //just a permission check
         }        
     }
     
